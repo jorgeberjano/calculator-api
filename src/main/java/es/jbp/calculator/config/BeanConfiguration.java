2f3947bf64e51f13f6c1cb38423cd79db68e3d74
Jorge Berjano <jorge.berjano@decidesoluciones.es>
2023-03-17 20:50:05 +0100

    Se añade la dependencia de la libreria de traza

---
+++ src/main/java/es/jbp/calculator/config/BeanConfiguration.java
@@ -0,0 +1,20 @@
+package es.jbp.calculator.config;
+
+import io.corp.calculator.TracerImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    /**
+     * Bean que implementa el sistema de traza.
+     * Realmente el tipo devuelto debería ser la interfaz TracerAPI, pero la clase TracerImpl no implementa dicha
+     * interfaz, se ve que es un error en la implementación de la librería tracer.jar
+     * @return El bean
+     */
+    @Bean
+    public TracerImpl tracer() {
+        return new TracerImpl();
+    }
+}
