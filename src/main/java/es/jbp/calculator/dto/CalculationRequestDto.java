cf3a67bb17ff231422ff45aaaafca334f5b3846a
Jorge Berjano <jorge.berjano@decidesoluciones.es>
2023-03-18 09:00:47 +0100

    Documentación open-api de los Dtos

--- src/main/java/es/jbp/calculator/dto/CalculationRequestDto.java
+++ src/main/java/es/jbp/calculator/dto/CalculationRequestDto.java
@@ -1,5 +1,6 @@
 package es.jbp.calculator.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,13 +9,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Schema(description="Request Dto for calculation operations")
 public class CalculationRequestDto {
 
+    @Schema(description="Unique request identifier for issue tracking", example = "9dbece61-2c25-4394-80fb-02a67d8c97dd")
     private String requestId;
 
+    @Schema(description="Operation type", example = "ADD or SUBTRACT")
     private String operationType;
 
+    @Schema(description="First operand for calculation")
     private String operand1;
 
+    @Schema(description="Second operand for calculation")
     private String operand2;
 }
--- src/main/java/es/jbp/calculator/dto/CalculationResponseDto.java
+++ src/main/java/es/jbp/calculator/dto/CalculationResponseDto.java
@@ -1,5 +1,6 @@
 package es.jbp.calculator.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -10,7 +11,9 @@ import lombok.*;
 @EqualsAndHashCode
 public class CalculationResponseDto {
 
+    @Schema(description="Unique request identifier for issue tracking", example = "9dbece61-2c25-4394-80fb-02a67d8c97dd")
     private String requestId;
 
+    @Schema(description="Result value of calculation")
     private String resultValue;
 }
